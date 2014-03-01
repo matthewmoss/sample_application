74da11def780e60cab78f57d2d40df2f991c6174
Your Name <matthewmoss@me.com>
2014-02-28 20:48:02 -0800

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ SampleApp::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  
+  config.force_ssl = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
